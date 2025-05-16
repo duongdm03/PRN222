58ec034fa2a9c0a1cbeda7ad073277f652628c36
duongdm03 <minhduongdo0903@gmail.com>
2025-05-16 16:48:28 +0700

    .

--- DemoTask-04/Program.cs
+++ DemoTask-04/Program.cs
@@ -1,35 +1,69 @@
-﻿var tasks = new List<Task<int>>();
-var source = new CancellationTokenSource();
-var token = source.Token;
-int completedIterations = 0;
-for (int n = 1; n <= 20; n++)
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading;
+
+class Program
 {
-    tasks.Add(Task.Run(() =>
+    static void Main()
     {
-        int interations = 0;
-        for (int ctr = 1; ctr <= 2_000_000; ctr++)
+        var tasks = new List<Task<int>>();
+        var source = new CancellationTokenSource();
+        var token = source.Token;
+        int completedIterations = 0;
+
+        for (int n = 1; n <= 20; n++)
         {
-            token.ThrowIfCancellationRequested();
-            interations++;
+            tasks.Add(Task.Run(() =>
+            {
+                int iterations = 0;
+                try
+                {
+                    for (int ctr = 1; ctr <= 2_000_000; ctr++)
+                    {
+                        token.ThrowIfCancellationRequested();
+                        iterations++;
+                    }
+
+                    // Chỉ tăng completed nếu hoàn thành hết vòng lặp
+                    Interlocked.Increment(ref completedIterations);
+                    if (completedIterations >= 10)
+                        source.Cancel();
+                }
+                catch (OperationCanceledException)
+                {
+                    // Task bị hủy, không làm gì thêm
+                }
+
+                return iterations;
+            }, token));
         }
-        Interlocked.Increment(ref completedIterations);
-        if (completedIterations >= 10)
-            source.Cancel();
-        return interations;
-    }, token));
-}
-Console.WriteLine("Waiting for the first 10 tasks to complete...\n");
-try
-{
-    Task.WaitAll(tasks.ToArray());
-}
-catch (AggregateException)
-{
-    Console.WriteLine("Status of tasks:\n");
-    Console.WriteLine("{0,10} {1,20} {2,14:N0}\n", "Task Id", "Status", "Interations");
-    foreach (var t in tasks)
-    {
-        Console.WriteLine("{0,10} {1,20} {1,14}", t.Id, t.Status, t.Status != TaskStatus.Canceled ? t.Result.ToString("N0") : "n/a");
+
+        Console.WriteLine("Waiting for the first 10 tasks to complete...\n");
+
+        try
+        {
+            Task.WaitAll(tasks.ToArray());
+        }
+        catch (AggregateException ae)
+        {
+            Console.WriteLine("One or more tasks were canceled or faulted.\n");
+        }
+
+        Console.WriteLine("Status of tasks:\n");
+        Console.WriteLine("{0,10} {1,20} {2,14}\n", "Task Id", "Status", "Iterations");
+
+        foreach (var t in tasks)
+        {
+            string result = "n/a";
+
+            if (t.Status == TaskStatus.RanToCompletion)
+                result = t.Result.ToString("N0");
+
+            Console.WriteLine("{0,10} {1,20} {2,14}", t.Id, t.Status, result);
+        }
+
+        Console.ReadLine();
     }
-    Console.ReadLine();
 }
